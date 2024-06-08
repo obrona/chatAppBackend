1165893b819490afb93bc11e9e3451504bd8779d
Tom <theodorelim2002@gmail.com>
2024-06-08 13:10:54 +0800

    change

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const Connection = require('./db.js')
 const mongoose = require('mongoose')
 const Chat = require('./models/Chat.js')
 const { timeStamp } = require('console')
+const cors = require('cors')
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 Connection()
 const server = http.createServer(app)
@@ -15,7 +17,8 @@ const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
         origin: '*'
-    }
+    },
+    
 })
 
 io.on('connection', (socket) => {
@@ -50,4 +53,4 @@ io.on('connection', (socket) => {
 const port = process.env.PORT || 3000
 //console.log(port)
 
-server.listen(port, () => console.log('running on port 3001'))
+server.listen(port, () => console.log(`running on port ${port}`))
